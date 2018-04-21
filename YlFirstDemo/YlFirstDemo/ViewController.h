8fe2a7aaf46223a0a401d03aeb05ee31274ceca5
liqiang <liqiang@zhihu.com>
2018-04-21 12:34:04 +0800

    创建的第一个工程

---
+++ YlFirstDemo/YlFirstDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YlFirstDemo
+//
+//  Created by kong on 2018/4/21.
+//  Copyright © 2018年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YlFirstDemo/YlFirstDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YlFirstDemo
+//
+//  Created by kong on 2018/4/21.
+//  Copyright © 2018年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
